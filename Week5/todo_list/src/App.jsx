418e924fcab49a8e28c5f2470002ac174548a3c3
Blazej3 <b.goszczyn88@gmail.com>
2024-03-10 14:34:50 +0200

    adding MUI

--- Week5/todo_list/src/App.jsx
+++ Week5/todo_list/src/App.jsx
@@ -1,43 +1,22 @@
-import { useState } from "react";
-import TodoTable from "./TodoTable";
+import './App.css';
+import TodoList from './ToDoList.jsx';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
-  const [todo, setTodo] = useState({ desc: "", date: "" });
-  const [todos, setTodos] = useState([]);
-
-  const addToDo = () => {
-    if (todo.desc && todo.date) {
-      setTodos([...todos, todo]);
-      setTodo({ desc: "", date: "" });
-    } else {
-      alert("missing data");
-    }
-  };
-
-  const deleteToDo = (index) => {
-    const filteredTodos = todos.filter((todo, i) => i !== index);
-    setTodos(filteredTodos);
-  };
-
   return (
-    <div>
-      <h1>Simple TodoList</h1>
-      <p>Add todo:</p>
-      <input
-        placeholder="Description"
-        onChange={(e) => setTodo({ ...todo, desc: e.target.value })}
-        value={todo.desc}
-      />
-      <input
-        type="date"
-        placeholder="Date"
-        onChange={(e) => setTodo({ ...todo, date: e.target.value })}
-        value={todo.date}
-      />
-      <button onClick={addToDo}>Add</button>
-      <TodoTable todos={todos} deleteToDo={deleteToDo} />
-    </div>
-  );
+    <>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">My Todos</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <TodoList />
+    </>
+  )
+
 }
 
 export default App;
--- Week5/todo_list/src/ToDoList.jsx
+++ Week5/todo_list/src/ToDoList.jsx
@@ -1,6 +1,14 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+import { DatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the grid
 
@@ -15,73 +23,92 @@ function ToDoList() {
   });
 
   const [todos, setTodos] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
 
 
   const [colDefs, setColDefs] = useState([
-    {field: 'desc', sortable: true, filter: true},
-    {field: 'priority', sortable: true, filter: true, 
-    cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'} },
-    
-    {field: 'date', sortable: true, filter: true}
+    { field: 'desc', sortable: true, filter: true },
+    {
+      field: 'priority', sortable: true, filter: true,
+      cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' }
+    },
+
+    { field: 'date', sortable: true, filter: true }
   ]);
+
+  const gridRef = useRef();
 
 
   const addToDo = () => {
     if (todo.desc && todo.date) {
       setTodos([...todos, todo]);
       setTodo({ desc: "", date: "", priority: "" });
+      setSelectedDate(null);
     } else {
       alert("missing data");
       setTodo({ desc: "", date: "", priority: "" });
     }
   };
-  const deleteToDo = (index) => {
-    console.log(index);
-    const filteredTodos = todos.filter((todo, i) => i !== index);
-    setTodos(filteredTodos);
+  const deleteToDo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(todos.filter((todo, index) =>
+        index != gridRef.current.getSelectedNodes()[0].id))
+    }
+    else {
+      alert('Select a row first!');
+    }
   };
 
 
 
   return (
-    <>
-      <h1>Simple Todolist</h1>
-      <p>Add todo:</p>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
 
-      <input
-        placeholder="Description"
-        onChange={(e) => setTodo({ ...todo, desc: e.target.value })}
-        value={todo.desc}
-      />
+      <Stack direction="row" spacing={2} mt={2} justifyContent="center" alignItems="center">
+        <TextField
+          label="Description"
+          onChange={(e) => setTodo({ ...todo, desc: e.target.value })}
+          value={todo.desc}
+        />
 
-      <input
-        placeholder="Priority"
-        onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
-        value={todo.priority}
-      />
-
-      <input
-        type="date"
-        placeholder="Date"
-        onChange={(e) => setTodo({ ...todo, date: e.target.value })}
-        value={todo.date}
-      />
-      <button onClick={addToDo}>Add</button>
+        <TextField
+          label="Priority"
+          onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
+          value={todo.priority}
+        />
 
 
 
+        <DatePicker
+          label="Date"
+          value={selectedDate}
+
+          onChange={(date) => { setSelectedDate(date); 
+          const dateString = date.toISOString().split('T')[0]; 
+          setTodo({ ...todo, date: dateString }); }}
+
+        />
+
+
+        <Button variant="contained" onClick={addToDo}>Add</Button>
+        <Button variant="contained" color="error" onClick={deleteToDo}>Delete</Button>
+
+      </Stack>
 
       <div
         className="ag-theme-material" // applying the grid theme
-        style={{ height: 600, width: 650}} // the grid will fill the size of the parent container
+        style={{ height: 600, width: 650 }} // the grid will fill the size of the parent container
       >
         <AgGridReact
+          ref={gridRef}
+          onGridReady={params => gridRef.current = params.api}
           rowData={todos}
           columnDefs={colDefs}
+          rowSelection="single"
         />
       </div>
 
-    </>
+    </LocalizationProvider>
   );
 }
 export default ToDoList;
